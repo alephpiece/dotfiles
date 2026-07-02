f2b929a42843521e0f9ef0e2c51de178ad67b63e
one <wangan.cs@gmail.com>
2026-07-03 00:19:38 +0800

    Add jq

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ esac
 if $DO_BIN; then
 
   # 1. Base tools ──────────────────────────────────────────────────────────────
-  ensure_apt_pkgs "base tools" tmux less curl git stow python3-pip time zstd
+  ensure_apt_pkgs "base tools" tmux less curl git stow python3-pip time zstd jq
 
   # 2. pipx ────────────────────────────────────────────────────────────────────
   if ! command -v pipx &>/dev/null; then
